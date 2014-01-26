3385adfb92be85ed75c72efd9c0cc5938d263a2b
JWhile <juloo.dsi@gmail.com>
2014-01-26 01:26:41 +0100

    Add class UU extends Builder & style .uu

--- uu-matter.js
+++ uu-matter.js
@@ -11,8 +11,24 @@ function UUMatter()
 {
     this.super('div');
 
+    this.uus = []; // :Array<UU>
+
     this.className('uu-matter')
         .append(new Builder('div')
             .className('uu-fabricator'));
 }
+// function spawnUU():void
+UUMatter.prototype.spawnUU = function()
+{
+    this.uus.push(new UU());
+};
 fus.extend(UUMatter, Builder);
+
+// class UU extends Builder
+function UU()
+{
+    this.super('span');
+
+    this.className('uu');
+}
+fus.extend(UU, Builder);
