46e7c78addd84a8607b19158f28332a1762f0699
Juloo <juloo.dsi@gmail.com>
2014-07-31 18:19:31 +0200

    Separe code

---
+++ js/entity.js
@@ -0,0 +1,97 @@
+/**
+ * UU-Matter <https://github.com/JWhile/UU-Matter>
+ *
+ * Copyright (c) 2014 juloo
+ *
+ * uu_matter.js
+ */
+
+function UUEntity(uuMatter)
+{
+	this.super('span');
+
+	this.uuMatter = uuMatter;
+	this.x = 0;
+	this.y = 0;
+	this.aX = (Math.random() * 2 - 1) / 16.6;
+	this.aY = (Math.random() * 2 - 1) / 16.6;
+	this.lastUpdate = Date.now();
+	this.exploded = false;
+	this.removed = false;
+
+	var self = this;
+
+	this.className('uu')
+		.event('mouseup', function()
+		{
+			if(!this.exploded)
+			{
+				self.click();
+				self.className('uu-ghost')
+					.setPos(15, 30);
+				setTimeout(function()
+				{
+					self.className('uu')
+						.css('opacity', '0');
+					setTimeout(function()
+					{
+						self.remove();
+					}, 450);
+				}, 200);
+			}
+		});
+}
+UUEntity.prototype.click = function()
+{
+	this.removed = true;
+	this.uuMatter.setScore(this.uuMatter.score + 1);
+};
+UUEntity.prototype.update = function()
+{
+	var now = Date.now();
+	var diff = now - this.lastUpdate;
+
+	this.setPos(this.aX * diff + this.x, this.aY * diff + this.y);
+	this.lastUpdate = now;
+};
+UUEntity.prototype.explode = function()
+{
+	this.exploded = true;
+	this.css('backgroundColor', 'rgba(255,10,10,0.7)')
+		.css('backgroundImage', 'none');
+
+	var self = this;
+
+	setTimeout(function()
+	{
+		self.removed = true;
+		self.remove();
+	}, 450);
+};
+UUEntity.prototype.collide = function(size)
+{
+	size -= 32;
+	return (this.x < 0 || this.x > size || this.y < 0 || this.y > size);
+};
+UUEntity.prototype.setPos = function(x, y)
+{
+	this.x = x;
+	this.y = y;
+	this.css('top', this.x +'px')
+		.css('left', this.y +'px');
+	return this;
+};
+fus.extend(UUEntity, Builder);
+
+function IridiumEntity(uuMatter)
+{
+	this.super(uuMatter);
+	this.css('backgroundImage', 'url("img/iridium.png")');
+}
+IridiumEntity.prototype.click = function()
+{
+	this.removed = true;
+	this.uuMatter.setScore(this.uuMatter.score + 1);
+	this.uuMatter.damage(-2);
+};
+fus.extend(IridiumEntity, UUEntity);
--- js/game.js
+++ js/game.js
@@ -3,10 +3,10 @@
  *
  * Copyright (c) 2014 juloo
  *
- * uu-matter.js
+ * game.js
  */
 
-function UUMatter()
+function UUMatterGame()
 {
 	this.super('div');
 
@@ -61,7 +61,7 @@ function UUMatter()
 
 	this.setScore(0);
 }
-UUMatter.prototype.start = function()
+UUMatterGame.prototype.start = function()
 {
 	this.menu.css('display', 'none');
 
@@ -106,7 +106,7 @@ UUMatter.prototype.start = function()
 	this.spawnUU();
 	this.fpsSpan.css('display', 'block');
 };
-UUMatter.prototype.stop = function()
+UUMatterGame.prototype.stop = function()
 {
 	this.playing = false;
 	this.menuBestScore.css('color', '#911');
@@ -120,7 +120,7 @@ UUMatter.prototype.stop = function()
 	this.menu.css('display', 'block');
 	this.fpsSpan.css('display', 'none');
 };
-UUMatter.prototype.update = function()
+UUMatterGame.prototype.update = function()
 {
 	for(var i = 0, uu; i < this.uus.length; ++i)
 	{
@@ -142,18 +142,18 @@ UUMatter.prototype.update = function()
 		}
 	}
 };
-UUMatter.prototype.setScore = function(score)
+UUMatterGame.prototype.setScore = function(score)
 {
 	this.score = score;
 	this.scoreSpan.css('display', (this.score === 0)? 'none' : 'block').text('x '+ this.score);
 };
-UUMatter.prototype.setBestScore = function(score)
+UUMatterGame.prototype.setBestScore = function(score)
 {
 	this.bestScore = score;
 	if(localStorage)
 		localStorage.setItem('UUMatterBestScore', this.bestScore);
 };
-UUMatter.prototype.damage = function(damage)
+UUMatterGame.prototype.damage = function(damage)
 {
 	if(damage >= this.life)
 	{
@@ -167,9 +167,9 @@ UUMatter.prototype.damage = function(damage)
 
 	this.css('boxShadow', '0 0 '+ Math.max(this.life * 6, 4) +'px #322,'+ size +' 0 0 #911,0 '+ size +' 0 #911,-'+ size +' 0 0 #911,0 -'+ size +' 0 #911,inset 0 0 '+ Math.max((20 - this.life) * 8, 0) +'px #700')
 };
-UUMatter.prototype.spawnUU = function()
+UUMatterGame.prototype.spawnUU = function()
 {
-	var uu = (Math.random() <= 0.05)? new Iridium(this) : new UU(this);
+	var uu = (Math.random() <= 0.05)? new IridiumEntity(this) : new UUEntity(this);
 	var pos = (564 - 32) / 2;
 
 	uu.setPos(pos, pos);
@@ -183,94 +183,4 @@ UUMatter.prototype.spawnUU = function()
 		self.uuFabricator.className('uu-fabricator');
 	}, 350);
 };
-fus.extend(UUMatter, Builder);
-
-function UU(uuMatter)
-{
-	this.super('span');
-
-	this.uuMatter = uuMatter;
-	this.x = 0;
-	this.y = 0;
-	this.aX = (Math.random() * 2 - 1) / 16.6;
-	this.aY = (Math.random() * 2 - 1) / 16.6;
-	this.lastUpdate = Date.now();
-	this.exploded = false;
-	this.removed = false;
-
-	var self = this;
-
-	this.className('uu')
-		.event('mouseup', function()
-		{
-			if(!this.exploded)
-			{
-				self.click();
-				self.className('uu-ghost')
-					.setPos(15, 30);
-				setTimeout(function()
-				{
-					self.className('uu')
-						.css('opacity', '0');
-					setTimeout(function()
-					{
-						self.remove();
-					}, 450);
-				}, 200);
-			}
-		});
-}
-UU.prototype.click = function()
-{
-	this.removed = true;
-	this.uuMatter.setScore(this.uuMatter.score + 1);
-};
-UU.prototype.update = function()
-{
-	var now = Date.now();
-	var diff = now - this.lastUpdate;
-
-	this.setPos(this.aX * diff + this.x, this.aY * diff + this.y);
-	this.lastUpdate = now;
-};
-UU.prototype.explode = function()
-{
-	this.exploded = true;
-	this.css('backgroundColor', 'rgba(255,10,10,0.7)')
-		.css('backgroundImage', 'none');
-
-	var self = this;
-
-	setTimeout(function()
-	{
-		self.removed = true;
-		self.remove();
-	}, 450);
-};
-UU.prototype.collide = function(size)
-{
-	size -= 32;
-	return (this.x < 0 || this.x > size || this.y < 0 || this.y > size);
-};
-UU.prototype.setPos = function(x, y)
-{
-	this.x = x;
-	this.y = y;
-	this.css('top', this.x +'px')
-		.css('left', this.y +'px');
-	return this;
-};
-fus.extend(UU, Builder);
-
-function Iridium(uuMatter)
-{
-	this.super(uuMatter);
-	this.css('backgroundImage', 'url("img/iridium.png")');
-}
-Iridium.prototype.click = function()
-{
-	this.removed = true;
-	this.uuMatter.setScore(this.uuMatter.score + 1);
-	this.uuMatter.damage(-2);
-};
-fus.extend(Iridium, UU);
+fus.extend(UUMatterGame, Builder);
